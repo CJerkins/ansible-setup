0a5ee26af813c59801669e557ed3da6e4a5d1c00
CJerkins <22862084+CJerkins@users.noreply.github.com>
2022-09-27 06:33:45 -0400

    added docker function

--- before_playbook.sh
+++ before_playbook.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
-
 sudo apt-get install ansible timeshift
 ansible-galaxy install geerlingguy.swap 
 ansible-galaxy install geerlingguy.clamav
 ansible-galaxy install geerlingguy.firewall
 ansible-galaxy install geerlingguy.security
-
+ansible-galaxy install geerlingguy.docker
 #sudo timeshift --create --comment "First Snapshot"
