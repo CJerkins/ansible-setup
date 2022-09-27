761444bb2dcff6b5ce826791282d0611a65e9f69
CJerkins <22862084+CJerkins@users.noreply.github.com>
2022-09-27 07:14:54 -0400

    Update after_playbook.sh

--- after_playbook.sh
+++ after_playbook.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-sudo apt-get install crowdsec
+sudo apt-get install crowdsec -y
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-sudo apt install crowdsec-firewall-bouncer-iptables
+sudo apt install crowdsec-firewall-bouncer-iptables -y
