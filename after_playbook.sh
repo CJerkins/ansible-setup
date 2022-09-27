b6f48ac8b4592c51836c4c1a17a2375be70f8816
CJerkins <22862084+CJerkins@users.noreply.github.com>
2022-09-27 06:17:56 -0400

    removed zeroteir

--- after_playbook.sh
+++ after_playbook.sh
@@ -1,7 +1,4 @@
 #! /bin/bash
-curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import && \
-if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
-sudo zerotier-cli join "networkID"
 
 curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
 sudo apt-get install crowdsec
